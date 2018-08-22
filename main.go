c0dbfe0c665e81e710bb85ad6fa5bc3326670b5a
W-B-S <36198591+W-B-S@users.noreply.github.com>
2018-08-22 16:52:35 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/W-B-S/nbs-node/utils/cmdKits"
+)
+
+func main() {
+
+	cmdKits.Execute()
+
+	//nbslog.Test()
+
+	//router := routing.GetInstance()
+	//router.Run()
+}
