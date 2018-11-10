fb32dc0ec69ba24db7ce31bd72a9418f316cbbaf
W-B-S <lws99927800@163.com>
2018-11-10 09:42:08 +0800

    just for a test.

---
+++ storage/network/shareport/impl_linux.go
@@ -0,0 +1,90 @@
+// +build darwin freebsd dragonfly netbsd openbsd linux
+
+package shareport
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"syscall"
+)
+
+const filePrefix = "sharePort."
+
+func socket(addr *syscall.SockaddrInet4) (int, error) {
+
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		return 0, err
+	}
+
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+		return 0, os.NewSyscallError("setSocketOption", err)
+	}
+
+	if err := syscall.Bind(fd, addr); err != nil {
+		return 0, fmt.Errorf("bind socket to file descrition error=%s", err.Error())
+	}
+
+	if err := syscall.SetNonblock(fd, true); err != nil {
+		return 0, fmt.Errorf("set noblock flag to file descrition error=%s", err.Error())
+	}
+
+	return fd, nil
+}
+
+func fdToPacketConn(fd int) (net.PacketConn, error) {
+
+	file := os.NewFile(uintptr(fd), filePrefix+strconv.Itoa(os.Getpid()))
+
+	packetConn, err := net.FilePacketConn(file)
+	if err != nil {
+		syscall.Close(fd)
+		return nil, err
+	}
+
+	if err := file.Close(); err != nil {
+		syscall.Close(fd)
+		packetConn.Close()
+		return nil, err
+	}
+
+	return packetConn, nil
+}
+
+func listenUDP(address *syscall.SockaddrInet4) (net.PacketConn, error) {
+
+	fd, err := socket(address)
+	if err != nil {
+		return nil, err
+	}
+
+	return fdToPacketConn(fd)
+}
+
+func dial(localAddr, remoteAddr *syscall.SockaddrInet4) (net.Conn, error) {
+	fd, err := socket(localAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := syscall.Connect(fd, remoteAddr); err != nil {
+		syscall.Close(fd)
+		return nil, err
+	}
+
+	file := os.NewFile(uintptr(fd), filePrefix+strconv.Itoa(os.Getpid()))
+	conn, err := net.FileConn(file)
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+
+	if err = file.Close(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
+}
