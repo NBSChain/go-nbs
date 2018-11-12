70d6d7f92c88df33fea84e99c2ce2c5b331bc14e
W-B-S <lws99927800@163.com>
2018-11-12 18:02:28 +0800

    test

--- storage/network/test/simplePeer.go
+++ storage/network/test/simplePeer.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/NBSChain/go-nbs/storage/network/pb"
 	"github.com/golang/protobuf/proto"
 	"net"
+	"syscall"
 	"time"
 )
 
@@ -15,15 +17,45 @@ func NewSimplePeer() *SimplePeer {
 	return &SimplePeer{}
 }
 
+func sharePort(network, address string, rawConn syscall.RawConn) error {
+	fn := func(s uintptr) {
+		if err := syscall.SetsockoptInt(syscall.Handle(s), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+			panic(err)
+		}
+		if err := syscall.SetNonblock(syscall.Handle(s), true); err != nil {
+			panic(err)
+		}
+	}
+	if err := rawConn.Control(fn); err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
 func (peer *SimplePeer) probe() error {
 
-	conn, err := net.DialUDP("udp4", nil, &net.UDPAddr{
-		IP:   net.ParseIP("192.168.103.101"),
-		Port: NatServerTestPort,
-	})
+	lc := &net.ListenConfig{
+		Control: sharePort,
+	}
+
+	_, err := lc.ListenPacket(context.Background(), "udp4", "0.0.0.0:7001")
+	if err != nil {
+		panic(err)
+	}
+
+	d := &net.Dialer{
+		Timeout: time.Second * 20,
+		LocalAddr: &net.UDPAddr{
+			Port: 7001,
+		},
+		Control: sharePort,
+	}
+
+	conn, err := d.Dial("udp4", "192.168.103.155:8001")
 
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	host, port, _ := net.SplitHostPort(conn.LocalAddr().String())
@@ -44,16 +76,31 @@ func (peer *SimplePeer) probe() error {
 
 	fmt.Println("simple peer probe......")
 
+	go read(conn)
+
 	for {
 
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 3)
 
-		conn.SetDeadline(time.Now().Add(time.Second * 5))
+		conn.SetWriteDeadline(time.Now().Add(time.Second * 2))
 		if no, err := conn.Write(requestData); err != nil || no == 0 {
 			fmt.Println("failed to send nat request to natServer ", err, no)
 			continue
 		}
 
+		fmt.Println("-request->done.")
+	}
+
+	return nil
+}
+
+func read(conn net.Conn) {
+
+	time.Sleep(time.Second * 2)
+
+	for {
+
+		conn.SetReadDeadline(time.Now().Add(time.Second * 5))
 		responseData := make([]byte, 2048)
 		hasRead, err := conn.Read(responseData)
 		if err != nil {
@@ -67,9 +114,6 @@ func (peer *SimplePeer) probe() error {
 			continue
 		}
 
-		fmt.Println(response)
-
+		fmt.Println("=======response=>", response)
 	}
-
-	return nil
 }
